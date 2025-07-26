1c31c8a3886d0830563bcfe68e1e775f05a4bf43
Kabir <44284877+0xk4b1r@users.noreply.github.com>
2025-07-27 00:02:46 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata, Viewport } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Navbar from "@/components/Navbar";
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({
     subsets: ['latin'],
@@ -190,6 +191,7 @@ export default function RootLayout({
                 <main>
                     {children}
                 </main>
+                <Analytics />
             </body>
         </html>
     )
